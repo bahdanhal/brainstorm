df10e337a3f567a7c00b7fe7fb0167035475bafd
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-19 17:40:21 +0300

    5

---
+++ task_5.php
@@ -0,0 +1,14 @@
+<?php 
+
+$digitsCount = 0;
+$N = $argv[1];
+$digits = array();
+
+while($N >= 1){
+    $digit = $N % 10;
+    $N /= 10;
+    $digits[$digitsCount++] = $digit;
+}
+for ($count = 0; $count < $digitsCount; $count++){
+    echo $digits[$count];    
+}
