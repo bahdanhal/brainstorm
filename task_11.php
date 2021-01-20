ac6e2bc20589278b58f81e5158eee8b8ad4f4960
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-20 15:36:58 +0300

    11

---
+++ task_11.php
@@ -0,0 +1,20 @@
+
+<?php 
+$N = $argv[1];  //multiplicator
+$M = $argv[2];  //max
+
+for($number = 1; $number <= $M; $number++){
+    $element = $dividend = $number * $N;
+    $sum = 0;
+
+    while($dividend >= 1){
+        $digit = $dividend % 10;
+        $dividend /= 10;
+        $sum += $digit;
+    }
+
+    //echo '/'.$number.'/';
+    if($element % $sum == 0){
+        echo $element. '   ';;
+    }
+}
