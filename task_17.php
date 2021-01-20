6f6c943612bcc56e62b766ce22422ad21113db7a
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-20 17:30:35 +0300

    17

---
+++ task_17.php
@@ -0,0 +1,26 @@
+<?php 
+$N = $argv[1];  //min
+$M = $argv[2];  //max
+if($N > $M){
+    $max = $N;
+    $min = $M;
+} else {
+    $max = $M;
+    $min = $N;
+}
+
+if($max % $min == 0){
+    exit($min);
+}
+
+$divisor = 1;
+$count = 2;
+$divCount = $min / 2;
+
+for($count = 2; $count <= $divCount; $count++){
+    if($max % $count == 0 and $min % $count == 0){
+        $divisor = $count;
+    }
+}
+
+echo $divisor;
