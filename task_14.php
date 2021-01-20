c4427149a8c40b464166d5e26e845f92fb4cb13b
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-20 16:25:29 +0300

    14

--- task_14.php
+++ task_14.php
@@ -1,21 +1,21 @@
-
 <?php 
-
-$number = $argv[1];
-$digitsCount = 0;
-$digits = array();
-
-while($number >= 1){
-    $digit = $number % 10;
-    $number /= 10;
-    $digits[$digitsCount++] = $digit;
+function simple($current)
+{
+    for($count = 2; $count <= sqrt($current); $count++){
+        if($current % $count == 0){
+            return false;
+        }
+    }
+    return true;
 }
-if($digitsCount % 2){
-    exit('no');
-}
-for ($count = 0; $count < $digitsCount/2; $count++){
-    if ($digits[$count] != $digits[$digitsCount - 1 - $count]){
-        exit('no');
+
+$maxPrime = 2;
+for($number = 0; $number < 10; $number++){
+    for($current = $maxPrime + 1; ;$current++){
+        if(simple($current) and simple($current + 2)){
+            echo $current . ' and ' . ($current + 2). PHP_EOL;
+            $maxPrime = $current + 2;
+            break;
+        }
     }
 }
-echo 'yes';
