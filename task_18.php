e401391729965b9564a3e0c9fa9c2d7929c68af4
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-20 18:08:41 +0300

    18

--- task_18.php
+++ task_18.php
@@ -1,7 +1,6 @@
 <?php 
-$N = $argv[1];  //min
-$M = $argv[2];  //max
-for($current = $N; $current <= $M; $current++){
+function sum($current)
+{
     $sum = 1;
     $count = 2; 
     for($count; $count < sqrt($current); $count++){
@@ -11,12 +10,37 @@ for($current = $N; $current <= $M; $current++){
         }
 
     }
-
     if($current % sqrt($current) == 0 and $count == sqrt($current)){
         $sum += sqrt($current);
     }
-    echo $sum.'_';
-    if($current == $sum){
-        echo $current. ' ';
+    return $sum;
+}
+
+$N = $argv[1]; //min
+$M = $argv[2]; //max
+$friends = array();
+$friendsCount = 0;
+for($firstNumber = $N; $firstNumber <= $M; $firstNumber++){
+    $firstSum = sum($firstNumber);
+    for($secondNumber = $N; $secondNumber <= $M; $secondNumber++){
+        if($secondNumber == $firstNumber){
+            break;
+        }
+
+        if(sum($secondNumber) == $firstNumber and $firstSum == $secondNumber){
+            $repeat = false;
+            for($currentFriendsCount = 0; $currentFriendsCount < $friendsCount; $currentFriendsCount++){
+                if(($secondNumber == $friends[$CurrentFriendsCount][0] and $firstNumber == $friends[$CurrentFriendsCount][1]) or 
+                ($secondNumber == $friends[$CurrentFriendsCount][1] and $firstNumber == $friends[$CurrentFriendsCount][0])){
+                    $repeat = true;
+                    break;
+                }
+            }
+            if($repeat){
+                break;
+            }
+            $friends[$friendsCount++] = [$firstNumber, $secondNumber];
+            echo $firstNumber. ' and '. $secondNumber. PHP_EOL;
+        }
     }
 }
