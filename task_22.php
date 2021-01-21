c0e11402fc092dd135ba527a267bfcd42262d521
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-21 10:11:40 +0300

    22

---
+++ task_22.php
@@ -0,0 +1,27 @@
+<?php
+
+function is_prime($number)
+{
+    for($count = 2; $count <= sqrt($number); $count++){
+        if($number % $count == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+$number = $argv[1];
+
+//while($number){
+    $count = 2; 
+    for($count; $count <= sqrt($number); $count++){
+        if(is_prime($count)){
+            if($number % $count == 0){
+                echo $count. ' ';
+                $number /= $count;
+                $count--;
+            }
+        }
+    }
+    echo $number;
+//}
