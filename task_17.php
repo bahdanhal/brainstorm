3111aa71d224c5cf1c1aaf7c67a284d76a86287f
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-21 09:42:19 +0300

    17 new alghorithm

--- task_17.php
+++ task_17.php
@@ -1,26 +1,12 @@
 <?php 
 $N = $argv[1];  //first number
 $M = $argv[2];  //second number
-if($N > $M){
-    $max = $N;
-    $min = $M;
-} else {
-    $max = $M;
-    $min = $N;
-}
 
-if($max % $min == 0){
-    exit($min);
-}
-
-$divisor = 1;
-$count = 2;
-$divCount = $min / 2;
-
-for($count = 2; $count <= $divCount; $count++){
-    if($max % $count == 0 and $min % $count == 0){
-        $divisor = $count;
+while($N and $M){
+    if($N > $M){
+        $N %= $M;
+    } else {
+        $M %= $N;
     }
 }
-
-echo $divisor;
+echo $N + $M;
