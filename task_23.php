6a13b4b256c6453f76838363c4141cfbf582ed18
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-21 17:34:17 +0300

    23 without variable-buffer

--- task_23.php
+++ task_23.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $number = $argv[1];
 $increase = 0;
 $count = 1;
@@ -34,22 +33,15 @@ while ($number >= 1 and $count > 1){
     $leftSide = (int) ($number / $leftDivider);
     $rightSide = $number % $rightDivider;
     $number = $leftSide * $rightDivider + $rightSide;
-    //echo $number. PHP_EOL;
+
     //add
-    //echo $count. PHP_EOL;
     $leftDivider = 10 ** ($count - 2);
     $rightDivider = 10 ** ($count - 2);
-    //echo $number. ' '. $leftDivider. '  '. $rightDivider. PHP_EOL;
     $leftSide = (int) ($number / $leftDivider);
-    
     $rightSide = $number % $rightDivider;
-    $number = $leftSide * $rightDivider * 10 + $min * (10 ** ($count - 2)) + $rightSide;
-    
-    //echo $leftSide. '  '. $min . ' '.$rightSide. PHP_EOL;
-    //echo $number. ' '. $min.'   '. (10 ** ($count - 2)). PHP_EOL;
-    
 
-    
+    $number = $leftSide * $rightDivider * 10 + $min * (10 ** ($count - 2)) + $rightSide;
+
     $count--;
 }
 echo $number;
