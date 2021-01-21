5c847d0195221963a31abcf16a363c08bb209831
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-21 17:33:27 +0300

    23 without variable-buffer

--- task_23.php
+++ task_23.php
@@ -3,44 +3,55 @@
 
 $number = $argv[1];
 $increase = 0;
-while ($number >= 1){
+$count = 1;
+
+$current = $number;
+while($current >= 1){
+    $current /= 10;
+    $count++;
+}
+while ($number >= 1 and $count > 1){
+    
     $current = $number;
     $min = $current % 10;
-    while($current >= 1){    //min finding
+    $currentRank = 1;
+    $rank = 1;
+    for($currentDigits = 0; $currentDigits < $count - 1; $currentDigits++){
+        
         $digit = $current % 10;
-      
+        
         if($digit < $min){
             $min = $digit;
+            $rank = $currentRank;
         }
+        $currentRank++;
         $current /= 10;
-    }
 
-    if(!$increase){     //result number
-        $increase = $min;
-    } else {
-        $increase *= 10;
-        $increase += $min;
     }
-    
-
-    $current = $number;
-    $rank = 1;
-    while ($current){   //rank of min finding
-        $digit = $current % 10;
-        if($digit == $min){
-            break;
-        }
-        $current /= 10;
-        $rank++;
-    }
-
+    //delete
     $leftDivider = 10 ** $rank;
     $rightDivider = 10 ** ($rank - 1);
     $leftSide = (int) ($number / $leftDivider);
     $rightSide = $number % $rightDivider;
-    //echo $number. '  '. $leftSide. ' '. $rightSide. ' '. $min;
     $number = $leftSide * $rightDivider + $rightSide;
-    //echo PHP_EOL;
-}
+    //echo $number. PHP_EOL;
+    //add
+    //echo $count. PHP_EOL;
+    $leftDivider = 10 ** ($count - 2);
+    $rightDivider = 10 ** ($count - 2);
+    //echo $number. ' '. $leftDivider. '  '. $rightDivider. PHP_EOL;
+    $leftSide = (int) ($number / $leftDivider);
+    
+    $rightSide = $number % $rightDivider;
+    $number = $leftSide * $rightDivider * 10 + $min * (10 ** ($count - 2)) + $rightSide;
+    
+    //echo $leftSide. '  '. $min . ' '.$rightSide. PHP_EOL;
+    //echo $number. ' '. $min.'   '. (10 ** ($count - 2)). PHP_EOL;
+    
 
-echo $increase;
+    
+    $count--;
+}
+echo $number;
+
+
---
+++ task_28.php
@@ -0,0 +1,45 @@
+<?php
+
+$number = $argv[1];
+$increase = 0;
+while ($number >= 1){
+    $current = $number;
+    $min = $current % 10;
+    while($current >= 1){    //min finding
+        $digit = $current % 10;
+      
+        if($digit < $min){
+            $min = $digit;
+        }
+        $current /= 10;
+    }
+
+    if(!$increase){     //result number
+        $increase = $min;
+    } else {
+        $increase *= 10;
+        $increase += $min;
+    }
+    
+
+    $current = $number;
+    $rank = 1;
+    while ($current){   //rank of min finding
+        $digit = $current % 10;
+        if($digit == $min){
+            break;
+        }
+        $current /= 10;
+        $rank++;
+    }
+
+    $leftDivider = 10 ** $rank;
+    $rightDivider = 10 ** ($rank - 1);
+    $leftSide = (int) ($number / $leftDivider);
+    $rightSide = $number % $rightDivider;
+    //echo $number. '  '. $leftSide. ' '. $rightSide. ' '. $min;
+    $number = $leftSide * $rightDivider + $rightSide;
+    //echo PHP_EOL;
+}
+
+echo $increase;
