599f9c88331184cb24f276aac39019d3b4280f2f
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-21 09:44:21 +0300

    19 new alghorithm

--- task_19.php
+++ task_19.php
@@ -1,25 +1,14 @@
 <?php 
 $N = $argv[1];  //first number
 $M = $argv[2];  //second number
-if($N > $M){
-    $max = $N;
-    $min = $M;
-} else {
-    $max = $M;
-    $min = $N;
-}
 
-$divisor = 1;
-$count = 2;
-$divCount = $min / 2;
-if($max % $min){
-    for($count = 2; $count <= $divCount; $count++){
-        if($max % $count == 0 and $min % $count == 0){
-            $divisor = $count;
-        }
+while($N and $M){
+    if($N > $M){
+        $N %= $M;
+    } else {
+        $M %= $N;
     }
-} else {
-    $divisor = $min;
 }
 
-echo $N * $M /$divisor;
+$divisor = $N + $M;
+echo $argv[1] * $argv[2] /$divisor;
