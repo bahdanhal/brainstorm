c45c00eddd0ecb706ff6f365ec6243b6e9362f6e
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-21 14:59:46 +0300

    25

---
+++ task_25.php
@@ -0,0 +1,27 @@
+<?php
+
+function factorial($number)
+{
+    return ($number == 1 or $number == 0) ? 1 : $number * factorial($number - 1);
+}
+
+$N = $argv[1];  //multipler
+$M = $argv[2];  //maximum
+$sum = 0;
+$amount = 0;
+for($count = 0; $count <= $M; $count++){
+    $current = $number = $N * $count;
+    $factSum = 0;
+    while($current >= 1){
+        $digit = $current % 10;
+        $factSum += factorial($digit);
+        $current /= 10;
+    }
+    if($factSum == $number){
+        echo $number.PHP_EOL;
+        $amount++;
+        $sum += $factSum;
+    }
+}
+echo 'sum '. $sum. ' ';
+echo 'amount '. $amount. ' ';
