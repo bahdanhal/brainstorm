e813857778536b9fc7706ac24978d48481c9be70
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-25 12:31:26 +0300

    54

---
+++ task_54.php
@@ -0,0 +1,40 @@
+<?php
+
+
+if($argc < 2){
+    exit;
+}
+
+$numbers = array();
+
+for($count = 1; $count < $argc; $count++){
+    $numbers[$count - 1] = $argv[$count];
+}
+$numbersCount = $argc - 1;
+$first = 0;
+$last = 0;
+$max = 0;
+$zeros = false;
+for($count = 0; $count < $numbersCount - 1; $count++){
+    $length = 0;
+    $currentFirst = 0;
+    $currentLast = 0;
+    if($numbers[$count] == 0){
+        $currentFirst = $count;
+        $zeros = true;
+    }
+
+    while($numbers[$count] == 0){
+        $currentLast = $count;
+        $count++;
+    }
+
+    if($currentLast - $currentFirst > $max){
+        $max = $currentLast - $currentFirst;
+        $first = $currentFirst;
+        $last = $currentLast;
+    }
+}
+if($zeros){
+    echo "first: $first, last: $last";
+}
