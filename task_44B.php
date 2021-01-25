079d3f06c4a02ea8f6116acf7c2e056c3fdbfc20
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-25 09:14:39 +0300

    44B

--- task_44B.php
+++ task_44B.php
@@ -16,37 +16,33 @@ for($count = 1; $count < $argc; $count++){
     $numbers[$count - 1] = $argv[$count];
 }
 $numbersCount = $argc - 1;
-$positive = array();
-$positiveCount = 0;
+$even = array();
+$evenCount = 0;
 
 for($count = 0; $count < $numbersCount; $count++){
-    if($numbers[$count] % 2){
-        break;
+    if(!($numbers[$count] % 2)){
+        continue;
     }
-    echo $numbers[$count]. '...';//. $numbers[$current].'/';
     $current = $count;
     for($next = $count + 1; $next < $numbersCount; $next++){
-        if($numbers[$next] % 2){
+        if(!($numbers[$next] % 2)){
             
-            $tmp = $numbers[$count];
-            $numbers[$count] = $numbers[$current];
+            $tmp = $numbers[$next];
+            $numbers[$next] = $numbers[$current];
             $numbers[$current] = $tmp;
+            break;
         }
     }
 
 }
 
-/*while($numbers[$numbersCount - 1] > 0){
-    $positive[$positiveCount++] = $numbers[$numbersCount - 1];
+while($numbers[$numbersCount - 1] % 2){
+    $even[$evenCount++] = $numbers[$numbersCount - 1];
     unset($numbers[$numbersCount - 1]);
     $numbersCount--;
 }
-while($numbers[$numbersCount - 1] == 0){
-    unset($numbers[$numbersCount - 1]);
-    $numbersCount--;
-}*/
 
-//echo 'Even: ';
+echo 'Even: ';
 printArray($numbers, $numbersCount);
-//echo 'Odd: ';
-//printArray($positive, $positiveCount);
+echo 'Odd: ';
+printArray($even, $evenCount);
