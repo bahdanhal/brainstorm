5781d31531ba21b00908bf631406b8310daa6200
bahdan.hal <bahdan.hal@hotmail.com>
2021-01-25 17:04:11 +0300

    4 fix

--- task_4.php
+++ task_4.php
@@ -1,43 +1,42 @@
 <?php 
-for($number = 1000; $number <10000; $number+=2){
+for($number = 1000; $number <10000; $number += 2){
     $current = $number;
     $prevDigit = $current % 10;
     $current /= 10;
     $digit = $current % 10;
+    $current /= 10;
     $direction = $digit <=> $prevDigit;
+    if($direction == 0){
+        continue;
+    }
+    
     $movement = false;
     while($current >= 1){
         $prevDigit = $digit;
         $digit = $current % 10;
+        
         switch($direction){
-            case 0:
-                $direction = $digit <=> $prevDigit;
-                $movement = true;
-                break;
-
             case -1:
+
                 $movement = true;
-                if ($digit > $prevDigit){
+                if ($digit >= $prevDigit){
                     $movement = false;
-                    $current = 0;
+                    break 2;
                 }
                 break;
             case 1:
                 $movement = true;
-                if ($digit < $prevDigit){
+                if ($digit <= $prevDigit){
                     $movement = false;
-                    $current = 0;
+                    break 2;
                 }
                 break;
-                
-        }
-
+        }  
+        
         $current /= 10;
 
     }
-
     if($movement){
         echo $number.' ';
     }
-    
 }
